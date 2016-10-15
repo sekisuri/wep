d2035afc0497108f56bc8f72157164a159db1fee
sekisuri <sekisuri@gmail.com>
2016-10-16 04:08:59 +0900

    프로필 사진추가 및 소스수정

--- av/index.php
+++ av/index.php
@@ -1,3 +1,8 @@
+<?php
+include "db/db_info.php";
+include "lib/config.php";
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,226 +21,59 @@
      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
    <![endif]-->
-   <link href="css/mpb.css" rel="stylesheet">
+   <link href="css/avrank.css" rel="stylesheet">
 
-   <link href="css/style.css" rel="stylesheet">
 
    	</head>
    	<body>
    		<!-- 헤더 -->
         <div class="header home">
-            <div class="menu-top-home">
-    			<div class="container">
-    				<div class="row">
-    					<div class="col-sm-3">
-    						<a href="http://symu.co" class="logo">
-    							<img src="https://symu.co/app/plugins/wp/resources/img/logo-small-white.png">
-    						</a>
-    					</div>
-    					<div class="col-sm-9 align-right">
-    						<ul class="menu-links">
-    							<li class="hidden-xs"><a href="#home">AV 배우목록</a></li>　
-    							<li class="hidden-xs"><a href="#features">AV 순위</a></li>
-    							<li class="hidden-xs"><a href="#">AV 작품</a></li>
-    							<li><a href="#loginModal" role="button" data-toggle="modal" class="btn-link">Log In</a></li>　
-    							<li><a href="#registerModal" role="button" data-toggle="modal" class="btn-danger">Sign Up</a></li>　
-    						</ul>
-    					</div>
-    				</div>
-    			</div>
-    		</div>
 
-    		<div class="menu-top-home-fixed hidden-xs">
-    			<div class="container">
-    				<div class="row">
-    					<div class="col-xs-3">
-    						<a href="http://symu.co" class="logo">
-    							<img src="https://symu.co/app/plugins/wp/resources/img/logo-small-black.png">
-    						</a>
-    					</div>
-    					<div class="col-xs-9 align-right">
-    						<ul class="menu-links">
-    							<li><a href="#home">AV 배우목록</a></li>
-    							<li><a href="#features">AV 순위</a></li>
-    							<li><a href="#">AV 작품</li>
-    							<li><a href="#loginModal" role="button" data-toggle="modal" class="btn-link">Log In</a></li>
-    							<li><a href="#registerModal" role="button" data-toggle="modal" class="btn-danger hidden-xs">Sign Up</a></li>
-    						</ul>
-    					</div>
-    				</div>
-    			</div>
-    		</div>
-    		<div class="container align-center">
-    			<h1><strong>Display your web design</strong><br>in a web browser</h1>
-    			<h4 class="silver hidden-xs">Fastest way to present your design and get feedback from your clients!</h4>
-    			<div class="uploader-box box-padding-big-top">
-    				<div class="btn-group">
-    					<span class="btn btn-primary fileinput-button btn-lg">
-    						<span>Upload Project</span>
-    						<input id="fileupload" type="file" name="files[]">
-    					</span>
-    				</div>
-    				<div id="progress" class="progress">
-    					<div class="progress-bar progress-bar-danger"></div>
-    				</div>
-    				<div id="files" class="files"></div>
-    			</div>
-    			<div class="silver small-info margin-top-big hidden-xs">
-    				Drop here or select your project<br>
-    				prepared in the PNG or JPG format
-    			</div>
-    		</div>
+
+
 
     	</div>
     	<div class="box-padding-big">
-            <div class="main_tab_list c7">
-		        <ul class="mactorul c7">
-                    <a href="http://www.mpb.kr/actor/1361">
-                        <li class="main_img">
-                            <img src="http://www.mpb.kr/profile/1361.jpg" data-original="http://www.mpb.kr/profile/1361.jpg" class="lazy" style="display: inline;">
-                        </li>
-                    <li class="main_title">
-                        <p><a href="http://www.mpb.kr/actor/1361" class="mactor">시노다 아유미</a></p><span>데뷰 : 2012/10/01<br>출연작품 : 209개</span>
+            <div class="rank_list c7">
+
+<?php
+
+//$query ="SELECT * FROM `av_model` ORDER BY av_id DESC LIMIT 1,10";
+$query = "SELECT * FROM av_model INNER JOIN mpb_rank ON av_model.av_krName = mpb_rank.av_krName ORDER BY  mpb_rank.rank_id";
+$result = mysql_query($query, $conn);
+while ($row=mysql_fetch_array($result)) {
+    # code...
+    $profile_pic = $site.$pic.$row['av_id'].".jpg";
+ ?>
+            <ul class="mactorul c7">
+                <a href="http://www.mpb.kr/actor/1361">
+                    <li class="main_img">
+                        <img src=<?=$profile_pic?> data-original=<?=$profile_pic?> class="lazy" style="display: inline;">
                     </li>
-				<li class="main_keyword">
+                <li class="main_title">
+                    <p><a href="http://www.mpb.kr/actor/1361" class="mactor"><?php echo $row['av_krName']; ?></a></p><span>데뷰 : <?php echo $row['av_debut'];?><br>출연작품 : 209개</span>
+                </li>
+                <li class="main_keyword">
                     <a href="http://www.mpb.kr/cup/I/1">
-                        <span class="dataHref" data-href="http://www.mpb.kr/cup/I/1">I컵</span></a>
+                        <span class="dataHref" data-href="http://www.mpb.kr/cup/I/1"><?=$row['av_bustcup']?></span></a>
                     <a href="http://www.mpb.kr/age/32/1">
                         <span class="dataHref" data-href="http://www.mpb.kr/age/32/1">32세</span></a>
                 </li>
-                <span class="ranking_icon">3위</span></a>
+                <span class="ranking_icon"><?=$row['rank_id']?>위</span></a>
             </ul>
+<?php
 
-                <ul class="mactorul c7">
-                    <a href="http://www.mpb.kr/actor/1016">
-                        <li class="main_img">
-                            <img src="http://www.mpb.kr/profile/1016.jpg" data-original="http://www.mpb.kr/profile/1016.jpg" class="lazy" style="display: inline;">
-                        </li>
-                        <li class="main_title">
-                            <p><a href="http://www.mpb.kr/actor/1016" class="mactor">츠보미</a></p>
-                            <span>데뷰 : 2008/02/22<br>출연작품 : 866개</span>
-                        </li>
-				        <li class="main_keyword">
-                            <a href="http://www.mpb.kr/cup/D/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/cup/D/1">D컵</span>
-                            </a>
-                            <a href="http://www.mpb.kr/age/30/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/age/30/1">30세</span>
-                            </a>
-                        </li>
-                        <span class="ranking_icon">4위</span>
-                    </a>
-                </ul>
+} // while($row=mysql_fetch_array($result))
 
-                <!-- -->
-                <ul class="mactorul c7">
-                    <a href="http://www.mpb.kr/actor/1016">
-                        <li class="main_img">
-                            <img src="http://www.mpb.kr/profile/1016.jpg" data-original="http://www.mpb.kr/profile/1016.jpg" class="lazy" style="display: inline;">
-                        </li>
-                        <li class="main_title">
-                            <p><a href="http://www.mpb.kr/actor/1016" class="mactor">츠보미</a></p>
-                            <span>데뷰 : 2008/02/22<br>출연작품 : 866개</span>
-                        </li>
-				        <li class="main_keyword">
-                            <a href="http://www.mpb.kr/cup/D/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/cup/D/1">D컵</span>
-                            </a>
-                            <a href="http://www.mpb.kr/age/30/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/age/30/1">30세</span>
-                            </a>
-                        </li>
-                        <span class="ranking_icon">4위</span>
-                    </a>
-                </ul>
 
-                <ul class="mactorul c7">
-                    <a href="http://www.mpb.kr/actor/1016">
-                        <li class="main_img">
-                            <img src="http://www.mpb.kr/profile/1016.jpg" data-original="http://www.mpb.kr/profile/1016.jpg" class="lazy" style="display: inline;">
-                        </li>
-                        <li class="main_title">
-                            <p><a href="http://www.mpb.kr/actor/1016" class="mactor">츠보미</a></p>
-                            <span>데뷰 : 2008/02/22<br>출연작품 : 866개</span>
-                        </li>
-				        <li class="main_keyword">
-                            <a href="http://www.mpb.kr/cup/D/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/cup/D/1">D컵</span>
-                            </a>
-                            <a href="http://www.mpb.kr/age/30/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/age/30/1">30세</span>
-                            </a>
-                        </li>
-                        <span class="ico_job">4위</span>
-                    </a>
-                </ul>
-
-                <ul class="mactorul c7">
-                    <a href="http://www.mpb.kr/actor/1016">
-                        <li class="main_img">
-                            <img src="http://www.mpb.kr/profile/1016.jpg" data-original="http://www.mpb.kr/profile/1016.jpg" class="lazy" style="display: inline;">
-                        </li>
-                        <li class="main_title">
-                            <p><a href="http://www.mpb.kr/actor/1016" class="mactor">츠보미</a></p>
-                            <span>데뷰 : 2008/02/22<br>출연작품 : 866개</span>
-                        </li>
-				        <li class="main_keyword">
-                            <a href="http://www.mpb.kr/cup/D/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/cup/D/1">D컵</span>
-                            </a>
-                            <a href="http://www.mpb.kr/age/30/1">
-                                <span class="dataHref" data-href="http://www.mpb.kr/age/30/1">30세</span>
-                            </a>
-                        </li>
-                        <span class="ico_job">4위</span>
-                    </a>
-                </ul>
-            </div>
+mysql_close( $conn);
+ ?>
+                    </div>
     	</div>
 
-    	<div class="box-padding-big white-bg">
 
-		<div class="profilebox sub_tab_content">
-			<span><img src="http://www.mpb.kr/profile/930.jpg" alt="우츠노미야 시온"></span>
-			<span>
-				<strong>
-					<table class="profile" id="actorProfile">
-						<tbody>
-                            <tr>
-                                <td class="f">이름</td>
-                                <td><b>우츠노미야 시온</b> / Shion Utsunomiya / 宇都宮しをん</td>
-                            </tr>
-							<tr>
-                                <td class="f">생년월일</td>
-                                <td>1994 년 03 월 01 일 ( 현재 <a href="http://www.mpb.kr/age/23/1"><font class="dataHref" data-href="http://www.mpb.kr/age/23/1">23 세</font></a> )</td>
-                            </tr>
-						    <tr>
-                                <td class="f">사이즈</td>
-                                <td>신장 161cm / 가슴 4 ( <a href="http://www.mpb.kr/cup/J/1"><font class="dataHref" data-href="http://www.mpb.kr/cup/J/1">J컵</font></a> ) / 허리 23 / 힙 36</td>
-                            </tr>
-						    <tr>
-                                <td class="f">공식데뷔</td>
-                                <td>2013년 09월 07일</td>
-                            </tr>
-					        <tr>
-                                <td class="f">다운로드</td>
-                                <td><a onclick="alert('다운로드는 회원전용 서비스이며, 해외에서 업로드 중이라 안전합니다! 아이디 비밀번호만 입력하면 가입완료!');" href="http://www.todisk.com/partner/?pid=boardphp&amp;mode=R" style="text-decoration:underline;" target="_blank">다운로드 바로가기</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
-				</strong>
-			</span>
-		</div>
 
-    	<div class="box-padding-big hidden-xs">
-
-    	</div>
-    	<div class="box-padding-big white-bg hidden-xs">
-
-    	</div>
-    	<div class="box-padding-big dark-bg-stat hidden-xs">
-
-    	</div>
+    
     	<div class="dark-footer">
 
     	</div>
