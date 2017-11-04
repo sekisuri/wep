f8474ea4309072dfdbb0113e1ba7d2e89ef6422c
sekisuri <sekisuri@gmail.com>
2017-11-04 20:29:38 +0000

    modify code

--- scrap/avlist/DB/SQL.py
+++ scrap/avlist/DB/SQL.py
@@ -16,3 +16,18 @@ def InsertName(avname):
     cur.execute(sql)
     cur.close()
     conn.close()
+
+def InsertProfile(data):
+	conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PASS,
+                                     db=DB, charset ='utf8')
+    cur = conn.cursor()
+
+	sql = "insert ignore into mpb_rank ( rank_id, hentaku_name,av_krName,av_enName,av_jaName,"
+	sql += "av_height,av_bust,av_waist,av_hip,av_bustcup) "
+    sql += "values ('{}','{}','{}',{})".format(
+            model['rank_id'],model['krName'],
+            model['rank_year'],model['rank_month'])
+    cur.execute(sql)
+    cur.close()
+    conn.close()
+	
--- scrap/avlist/avdbs_profile.py
+++ scrap/avlist/avdbs_profile.py
@@ -1,7 +1,8 @@
  #-*- coding: utf-8 -*-
+import re
 import requests
 import urllib.request
-from DB.SQL import InsertName
+#from DB.SQL import InsertName
 from bs4 import BeautifulSoup
 #url = "http://192.168.0.7/downW
 '''
@@ -20,51 +21,61 @@ for i in range(1,2721):
     
 
 '''
+decimal_regex = r'\d' #정규표현식 숫자만
+string_regex = '[A-Z]+' #정규표현식 대문자 알파벳 ..
 url = 'http://www.avdbs.com/menu/subpage04/profile_view.php?actor_idx=285'	
 html = requests.get(url).text
 soup = BeautifulSoup(html)
 def av_profile():
 	avRank = {}
-	'''
-	for member_tag in soup.select('.av_category_path'):
-		#rank = member_tag.find('div',class_='avrank').text
-		hentakuName = member_tag.find('li',class_='first_path').find('h1')
-		print(hentakuName)
-		#avRank['rank_id'] = rank
-		#avRank['krName'] = avname
-		#avRank['rank_year'] = "2016"
-		#avRank['rank_month'] = '10'
-		#InsertRank(avRank)
-	'''
-	
-	
+
 	
 	av_name = soup.find('div',class_="profile_view_inner")
-	av_pic1 = av_name.img
+	main_pic = av_name.img.get('src') # img 태그의 src 속성을 가져온다.
 	kr_name = av_name.find('span',class_="inner_name_kr").text
 	hentaku_name = kr_name.replace(" ","") #공백제거
 	en_name = av_name.find('span',class_="inner_name_en").text
 	cn_name = soup.find('span',class_="inner_name_cn").text
-	print(hentaku_name)
-	print(kr_name)
-	print(en_name)
-	print(cn_name)
-	print(av_pic1)
+	print(hentaku_name) #hentaku 에서 인식될 한국어이름
+	print(kr_name) # 한국어 이름
+	print(en_name) # 영어 이름
+	print(cn_name) # 일본 이름
+	print(main_pic) # 사진
 	
 	profile = soup.find('p',class_="profile_bottom_text")
-	
-	
 	pData = []
 	
 	i = 0
-	for rp in profile.find_all('span'):
+	for rp in profile.find_all('span'): #프로필 데이터를 가져오기 위해 반복문
 		pData.append(rp.text)
 		#print(pProfile[i])
 		i = i + 1
 	
-	print(pData[0])
-	print(pData[1])
-	print(pData[2])
-	print(pData[4])
+	
+	pbirth = "" . join(re.findall(decimal_regex,pData[0])) #생일
+	pheight = "" . join(re.findall(decimal_regex,pData[1])) #키
+	psize = "" . join(re.findall(decimal_regex,pData[2])) #신체사이즈
+	pcupsize = "" . join(re.findall(string_regex,pData[3]))#가슴 컵
+	pdebut = "" . join(re.findall(decimal_regex,pData[4])) #데뷰일
 
+	print(pbirth)
+	print(pheight)
+	print(psize)
+	print(pcupsize)
+	print(pdebut)
+	
+	# 기타 이미지를 가져온다.
+	otherpic = []
+	j = 0
+	apic = soup.find('div',class_="other_photo_list")
+	for ppic in apic.find_all('li'):
+		if 'go_popup' in ppic.a.get('onclick'): # 끝 부분에 광고 주소가 들어가기 때문에 'go_popup'를 중심으로 걸려낸다.
+			otherpic.append(ppic.a.get('onclick').replace("go_popup('","").replace("');",""))
+
+			print(otherpic[j])
+		j = j + 1
+	
+	#avpic1 = otherpic[0].replace("');")
+	
+	
 av_profile()
