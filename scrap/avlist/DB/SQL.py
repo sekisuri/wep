76e7db0da6c9499963e751456cf746a781c1093b
sekisuri <sekisuri@gmail.com>
2017-11-06 02:26:56 +0000

    scrap in db

--- scrap/avlist/DB/SQL.py
+++ scrap/avlist/DB/SQL.py
@@ -18,16 +18,19 @@ def InsertName(avname):
     conn.close()
 
 def InsertProfile(data):
-	conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PASS,
-                                     db=DB, charset ='utf8')
-    cur = conn.cursor()
+	conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PASS, db=DB, charset ='utf8')
+	sqlconn = conn.cursor()
 
-	sql = "insert ignore into mpb_rank ( rank_id, hentaku_name,av_krName,av_enName,av_jaName,"
-	sql += "av_height,av_bust,av_waist,av_hip,av_bustcup) "
-    sql += "values ('{}','{}','{}',{})".format(
-            model['rank_id'],model['krName'],
-            model['rank_year'],model['rank_month'])
-    cur.execute(sql)
-    cur.close()
-    conn.close()
+	sql = "insert ignore into av_profile (avdb_id,hentaku_name, av_krName, av_enName, av_jaName,"
+	sql += "av_height, av_size, av_bustcup, av_birthday, av_debut, main_pic,"
+	sql += "av_pic1, av_pic2, av_pic3, av_pic4, av_pic5 ) "
+	sql += "values ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(
+		data['avdb'],data['hentaku'],data['krname'],data['enname'],data['janame'],
+		data['height'],data['size'],
+		data['bustcup'],data['birthday'],data['debut'],data['mainpic'],data['pic1'],
+		data['pic2'],data['pic3'],data['pic4'],data['pic5'])
+		
+	sqlconn.execute(sql)
+	sqlconn.close()
+	conn.close()
 	
--- scrap/avlist/avdbs_profile.py
+++ scrap/avlist/avdbs_profile.py
@@ -2,49 +2,52 @@
 import re
 import requests
 import urllib.request
-#from DB.SQL import InsertName
+import time
+from DB.SQL import InsertProfile
 from bs4 import BeautifulSoup
 #url = "http://192.168.0.7/downW
-'''
-for i in range(1,2721): 
-    
-    url = 'http://www.avdbs.com/menu/subpage04/profile_view.php?actor_idx=' + str(i)
-    html = requests.get(url).text
-    soup = BeautifulSoup(html)
-    print(url)
-    count = 0
-    for member_tag in soup.select('.list a'):
-        count = count + 1
-        link = member_tag['href']
-        #print(link.split('/')[2])
-        InsertName(link.split('/')[2])
-    
-
-'''
 decimal_regex = r'\d' #정규표현식 숫자만
 string_regex = '[A-Z]+' #정규표현식 대문자 알파벳 ..
-url = 'http://www.avdbs.com/menu/subpage04/profile_view.php?actor_idx=285'	
-html = requests.get(url).text
-soup = BeautifulSoup(html)
-def av_profile():
-	avRank = {}
 
+def av_profile(index):
 	
+	url = 'http://www.avdbs.com/menu/subpage04/profile_view.php?actor_idx=' + str(index)
+	html = requests.get(url).text
+	soup = BeautifulSoup(html)
+	insertDB = {} #디비에 넣기위해	
+	otherpic = [] # 기타 이미지를 가져온다.
+
+# 에러를 피하기 위해 변수에 초기값 초기값을 넣는다. 
+	av_name = "?"
+	main_pic = "http://addingfriends.net/image/blank.jpg"
+	kr_name = "?"
+	hentaku_name = kr_name.replace(" ","") #공백제거
+	en_name = "?"
+	cn_name = "?"
+	pbirth = "00000000"
+	pheight = "000"
+	psize = "000000"
+	pcupsize = "?"
+	pdebut = "0000"
+	for i in range(0,5):
+		otherpic.append("http://addingfriends.net/image/blank.jpg")
+# 변수 초기화 끝
 	av_name = soup.find('div',class_="profile_view_inner")
 	main_pic = av_name.img.get('src') # img 태그의 src 속성을 가져온다.
 	kr_name = av_name.find('span',class_="inner_name_kr").text
 	hentaku_name = kr_name.replace(" ","") #공백제거
 	en_name = av_name.find('span',class_="inner_name_en").text
 	cn_name = soup.find('span',class_="inner_name_cn").text
-	print(hentaku_name) #hentaku 에서 인식될 한국어이름
-	print(kr_name) # 한국어 이름
-	print(en_name) # 영어 이름
-	print(cn_name) # 일본 이름
-	print(main_pic) # 사진
+	
+	insertDB['avdb'] = index
+	
+	insertDB['hentaku'] = hentaku_name
+	insertDB['krname'] = kr_name
+	insertDB['enname'] = en_name
+	insertDB['janame'] = cn_name
 	
 	profile = soup.find('p',class_="profile_bottom_text")
-	pData = []
-	
+	pData = []	
 	i = 0
 	for rp in profile.find_all('span'): #프로필 데이터를 가져오기 위해 반복문
 		pData.append(rp.text)
@@ -58,24 +61,72 @@ def av_profile():
 	pcupsize = "" . join(re.findall(string_regex,pData[3]))#가슴 컵
 	pdebut = "" . join(re.findall(decimal_regex,pData[4])) #데뷰일
 
-	print(pbirth)
-	print(pheight)
-	print(psize)
-	print(pcupsize)
-	print(pdebut)
 	
-	# 기타 이미지를 가져온다.
-	otherpic = []
+	insertDB['height'] = pheight
+	insertDB['size'] = psize
+	insertDB['bustcup'] = pcupsize
+	insertDB['birthday'] = pbirth
+	insertDB['debut'] = pdebut
+	
+	
+	
 	j = 0
 	apic = soup.find('div',class_="other_photo_list")
 	for ppic in apic.find_all('li'):
 		if 'go_popup' in ppic.a.get('onclick'): # 끝 부분에 광고 주소가 들어가기 때문에 'go_popup'를 중심으로 걸려낸다.
-			otherpic.append(ppic.a.get('onclick').replace("go_popup('","").replace("');",""))
+			otherpic.insert(j, ppic.a.get('onclick').replace("go_popup('","").replace("');",""))
 
-			print(otherpic[j])
+			#print(otherpic[j])
 		j = j + 1
 	
 	#avpic1 = otherpic[0].replace("');")
+	''''''
+	insertDB['mainpic'] = main_pic
+	insertDB['pic1'] = otherpic[0]
+	insertDB['pic2'] = otherpic[1]
+	insertDB['pic3'] = otherpic[2]
+	insertDB['pic4'] = otherpic[3]
+	insertDB['pic5'] = otherpic[4]
+	InsertProfile(insertDB)
 	
-	
-av_profile()
+	'''
+	insertDB['avdb'] = 285
+	insertDB['mainpic'] = main_pic
+	insertDB['hentaku'] = hentaku_name
+	insertDB['krname'] = kr_name
+	insertDB['enname'] = en_name
+	insertDB['janame'] = cn_name
+	insertDB['height'] = pheight
+	insertDB['bust'] = psize[0:2]
+	insertDB['waist'] = psize[2:4]
+	insertDB['hip'] = psize[4:6]
+	insertDB['bustcup'] = pcupsize
+	insertDB['birthday'] = pbirth
+	insertDB['debut'] = pdebut
+	insertDB['pic1'] = otherpic[0]
+	insertDB['pic2'] = otherpic[1]
+	insertDB['pic3'] = otherpic[2]
+	insertDB['pic4'] = otherpic[3]
+	insertDB['pic5'] = otherpic[4]
+	print(insertDB['avdb'])
+	print(insertDB['mainpic'])
+	print(insertDB['hentaku'])
+	print(insertDB['krname'])
+	print(insertDB['enname'])
+	print(insertDB['janame'])
+	print(insertDB['height'])
+	print(insertDB['size'])
+	print(insertDB['bustcup'])
+	print(insertDB['birthday'])
+	print(insertDB['debut'])
+	print(insertDB['pic1'])
+	print(insertDB['pic2'])
+	print(insertDB['pic3'])
+	print(insertDB['pic4'])
+	print(insertDB['pic5'])
+	'''
+
+for i in range(1,2721): 
+	av_profile(i)
+	print(i)
+	time.sleep(3) # 3초간 쉰다.
