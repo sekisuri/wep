1a340ed5aab4dbab6675ebf6a5ac0d843e2e026f
sekisuri <sekisuri@gmail.com>
2019-06-12 07:23:18 +0900

    아규먼트를 받아 선택적으로 코드조각 날짜를 수정한다

--- tododate.py
+++ tododate.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 
 now = datetime.now()
@@ -5,7 +6,6 @@ ftodo1 = "C:\\Users\\wizar\\OneDrive\\문서\\MySnippet\\myTodo.snippet"
 ftodo2 = "C:\\Users\\wizar\\OneDrive\\문서\\MySnippet\\myTodo2.snippet"
 ftodo3 = "C:\\Users\\wizar\\OneDrive\\문서\\MySnippet\\myTodo3.snippet"  
 
-f = open(ftodo2, 'w+',encoding='UTF8')
 
 settime = "%s-%s-%s" %(now.year,now.month,now.day)
 todo1 = '''<?xml version="1.0" encoding="utf-8"?>
@@ -63,8 +63,9 @@ todo3 = '''<?xml version="1.0" encoding="utf-8"?>
             <Description>클래서 설명 주석</Description>
             <Author>sekisuri</Author>
             <SnippetTypes>
-            <SnippetType>Expansion</SnippetType>
-            <SnippetType>SurroundsWith</SnippetType>
+                <SnippetType>Expansion</SnippetType>
+                <SnippetType>SurroundsWith</SnippetType>
+             </SnippetTypes>
         </Header>
             <Snippet>
                 <Code Language="csharp">
@@ -80,6 +81,43 @@ todo3 = '''<?xml version="1.0" encoding="utf-8"?>
     </CodeSnippets>
     ''' % settime
 
-#f.write(todo2)
-print(settime)
-f.close()
+def main():
+    parser = argparse.ArgumentParser()
+    parser .add_argument("op",type=str,
+    choices=['todo1','todo2','todo3','all'],
+    help="choice todo1 , todo2 , todo3 , all")
+    args = parser.parse_args()
+    opstr = args.op
+    makerun(opstr)
+   
+
+def makerun(op):
+    if op == 'todo1':
+        f = open(ftodo1,"w+",encoding="utf-8")
+        f.write(todo1)
+        f.close()
+    elif op == 'todo2':
+        f = open(ftodo2,"w+",encoding="utf-8")
+        f.write(todo2)
+        f.close()
+    elif op == 'todo3':
+        f = open(ftodo3,"w+",encoding="utf-8")
+        f.write(todo3)
+        f.close()
+    elif op == 'all':
+        f1 = open(ftodo1,"w+",encoding="utf-8")
+        f1.write(todo1)
+        f1.close()
+        f2 = open(ftodo2,"w+",encoding="utf-8")
+        f2.write(todo2)
+        f2.close()
+        f3 = open(ftodo1,"w+",encoding="utf-8")
+        f3.write(todo3)
+        f3.close()
+    else :
+        print("arg error")
+
+
+if __name__=="__main__":
+    main()
+
