c0b6266efe6bb1b0c808796f05fa980e387bd947
sekisuri - GitHub <sekisuri@gmail.com>
2020-03-19 08:39:33 +0900

    반복문 추가

--- bookprint.py
+++ bookprint.py
@@ -1,13 +1,18 @@
 from PIL import ImageGrab
+import pyautogui
 import time
-import os
+
 
 path = "1.png"
 
-print(os.getcwd())
-now = time.localtime()
-time = "%04d-%02d-%02d-%02dh-%02dm-%02ds" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+print("3초 후 시작")
+time.sleep(3)
  
-img=ImageGrab.grab()
-saveas=path 
-img.save(saveas)
+for i in range(5):
+    time.sleep(3)
+    img=ImageGrab.grab()
+    saveas= str(i) + ".png" 
+    img.save(saveas)
+    pyautogui.scroll(100)
+
+
