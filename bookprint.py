42a9cdad99a777dcd936226540a9cc9f2cf743dd
sekisuri <sekisuri@gmail.com>
2020-03-19 21:00:43 +0900

    키보드제어

--- bookprint.py
+++ bookprint.py
@@ -2,9 +2,6 @@ from PIL import ImageGrab
 import pyautogui
 import time
 
-
-path = "1.png"
-
 print("3초 후 시작")
 time.sleep(3)
  
@@ -12,7 +9,13 @@ for i in range(5):
     time.sleep(3)
     img=ImageGrab.grab()
     saveas= str(i) + ".png" 
-    img.save(saveas)
-    pyautogui.scroll(100)
+   # img.save(saveas)
+    print(saveas)
+   # pyautogui.scroll(100)
+   # pyautogui.press('rigit')
+    pyautogui.keyDown('right')
+    time.sleep(1)
+    pyautogui.keyUp('right')
+
 
 
