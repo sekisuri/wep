828dfc106a1023f2cdcc90b8fdfa92eafc4b9a09
sekisuri <sekisuri@gmail.com>
2020-05-02 23:38:01 +0900

    first commit

---
+++ ch01/main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+      // 객체 데이터
+      message: {
+        value: 'Hello Vue.js!'
+      },
+      // 배열 데이터
+      list: ['사과', '바나나', '딸기'],
+      // 4번을 위한 요소
+      num: 1
+    }
+  })
